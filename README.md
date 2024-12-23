b2ef0bf542fecacf34dc48ab87354143d31accc4
TomCo2210 <tomco@outlook.co.il>
2024-12-23 14:50:31 +0200

    api upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# 25A-10221-FeatureToggl_Flask_API
+
+- Flask
+- Flasgger (Flask Swagger)
+- MongoDB Connection
+- HTTP 
+- Vercel Deployment
---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flasgger import Swagger
+from mongodb_connection_manager import MongoConnectionManager
+from routes import initial_routes
+import os
+
+app = Flask(__name__)
+Swagger(app)
+
+# Initialize Database Connection
+MongoConnectionManager.initialize_db()
+
+# Import the routes
+initial_routes(app)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8088))
+    app.run(debug=True, port=port)
